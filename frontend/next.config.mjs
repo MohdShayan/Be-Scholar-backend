de2dc45853fe24e198f8e4f7fdfd3ef709d86cdb
Mohd. Shayan <70843731+MohdShayan@users.noreply.github.com>
2024-10-09 22:58:31 +0530

    Delete frontend directory

--- frontend/next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        domains: ['t3.ftcdn.net', 'drive.google.com','media.gettyimages.com'], // Add all domains in one array
-    },
-};
-
-export default nextConfig;
